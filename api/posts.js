a0f2f34f81fd5811e9edb23259e23643bcdb5c63
BryanHuerta <bhuertar@DESKTOP-OUBEMJ8>
2024-04-25 22:43:15 -0500

    log in routes

--- api/posts.js
+++ api/posts.js
@@ -2,7 +2,7 @@ const postsRouter = require('express').Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// Get Posts Route
+// Get Post Routes
 postsRouter.get('/', async (req, res, next) => {
   try {
     const allPosts = await prisma.post.findMany();
@@ -24,5 +24,64 @@ postsRouter.get('/:id', async (req, res, next) => {
     next(error);
   }
 })
+
+// Signed In Routes
+postsRouter.use('/', (req, res, next) => {
+  if(!req.user) {
+    return res.status(401).send('You must be logged in to do that');
+  }
+  next();
+})
+
+postsRouter.post('/', async(req, res, next) => {
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title: req.body.title,
+        content: req.body.content,
+        userId: req.user.id
+      }
+    })
+    res.status(201).send(post);
+  } catch(error) {
+    next(error);
+  }
+})
+
+postsRouter.put('/:id', async(req, res, next) => {
+  try {
+    const post = await prisma.post.update({
+      data: {
+        title: req.body.title,
+        content: req.body.content
+      },
+      where: {
+        id: req.params.id*1,
+        userId: req.user.id
+      }
+    })
+    res.status(201).send(post);
+  } catch(error) {
+    next(error);
+  }
+})
+
+
+postsRouter.delete('/:id', async(req, res, next) => {
+  try {
+    const post = await prisma.post.delete({
+      where: {
+        id: req.params.id*1,
+        userId: req.user.id
+      }
+    })
+    res.status(201).send(post);
+  } catch(error) {
+    next(error);
+  }
+})
+
+
+
 
 module.exports = postsRouter;
